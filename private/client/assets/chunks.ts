da6582b8f2b3c55f52c4a6e7febddeb93c40fd86
Leo Lamprecht <leo@ronin.co>
2025-04-09 16:31:38 +0200

    Revert "Always hydrate root"
    This reverts commit d8924bf9787aaddd61bac3ad9c932dade87cda4c.

--- private/client/assets/chunks.ts
+++ private/client/assets/chunks.ts
@@ -5,14 +5,16 @@ import { hydrateRoot } from 'react-dom/client';
 
 import fetchPage from '../utils/fetch-page';
 
-const path = location.pathname + location.search + location.hash;
+if (!window['BLADE_ROOT']) {
+  const path = location.pathname + location.search + location.hash;
 
-fetchPage(path).then((page) => {
-  if (!page) throw new Error('Fetched page missing for initial render.');
+  fetchPage(path).then((page) => {
+    if (!page) throw new Error('Fetched page missing for initial render.');
 
-  window['BLADE_ROOT'] = hydrateRoot(document, page.body, {
-    onRecoverableError(error, errorInfo) {
-      console.error('Hydration error occurred:', error, errorInfo);
-    },
+    window['BLADE_ROOT'] = hydrateRoot(document, page.body, {
+      onRecoverableError(error, errorInfo) {
+        console.error('Hydration error occurred:', error, errorInfo);
+      },
+    });
   });
-});
+}
