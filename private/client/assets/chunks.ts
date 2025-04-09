d8924bf9787aaddd61bac3ad9c932dade87cda4c
Leo Lamprecht <leo@ronin.co>
2025-04-09 16:25:15 +0200

    Always hydrate root

--- private/client/assets/chunks.ts
+++ private/client/assets/chunks.ts
@@ -5,16 +5,14 @@ import { hydrateRoot } from 'react-dom/client';
 
 import fetchPage from '../utils/fetch-page';
 
-if (!window['BLADE_ROOT']) {
-  const path = location.pathname + location.search + location.hash;
+const path = location.pathname + location.search + location.hash;
 
-  fetchPage(path).then((page) => {
-    if (!page) throw new Error('Fetched page missing for initial render.');
+fetchPage(path).then((page) => {
+  if (!page) throw new Error('Fetched page missing for initial render.');
 
-    window['BLADE_ROOT'] = hydrateRoot(document, page.body, {
-      onRecoverableError(error, errorInfo) {
-        console.error('Hydration error occurred:', error, errorInfo);
-      },
-    });
+  window['BLADE_ROOT'] = hydrateRoot(document, page.body, {
+    onRecoverableError(error, errorInfo) {
+      console.error('Hydration error occurred:', error, errorInfo);
+    },
   });
-}
+});
