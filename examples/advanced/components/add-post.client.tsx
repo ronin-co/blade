1208c6a6951a77019c435e57c0c08a650bc22f65
Ben <4991309+NuroDev@users.noreply.github.com>
2025-04-24 18:00:30 +0100

    Add advanced example (#53)

---
+++ examples/advanced/components/add-post.client.tsx
@@ -0,0 +1,19 @@
+import { useMutation } from '@ronin/blade/client/hooks';
+
+export const AddPostButton = () => {
+  const { add } = useMutation();
+
+  return (
+    <button
+      onClick={async () => {
+        await add.post.with({
+          title: 'New Post',
+          body: 'This is a new post.',
+          userId: 1,
+        });
+      }}
+      type="button">
+      Add Post
+    </button>
+  );
+};
---
+++ examples/advanced/pages/index.tsx
@@ -0,0 +1,8 @@
+const Page = () => (
+  <>
+    <h1>Hello World</h1>
+    <a href="/posts">/posts</a>
+  </>
+);
+
+export default Page;
