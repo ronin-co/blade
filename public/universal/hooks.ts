58f33cf913cb379e57b0658c89b7f77e7e1ce76c
Leo Lamprecht <leo@ronin.co>
2025-04-27 15:41:04 +0200

    Make redirects work again

--- public/universal/hooks.ts
+++ public/universal/hooks.ts
@@ -81,7 +81,7 @@ const useRedirect = () => {
 
     transitionPage(populatedPathname, 'manual', {
       immediatelyUpdateQueryParams: options?.immediatelyUpdateQueryParams,
-    });
+    })();
   };
 };
 
