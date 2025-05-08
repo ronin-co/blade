360547bbfe02abf8e19d9773f2e569aeda23afb7
Leo Lamprecht <leo@ronin.co>
2025-05-08 09:59:07 +0200

    Ensure clean IDs

--- private/client/utils/wrap-client.ts
+++ private/client/utils/wrap-client.ts
@@ -4,12 +4,15 @@ const CLIENT_REFERENCE = Symbol.for('react.client.reference');
 
 export const wrapClientComponent = (component: ComponentType<any>, name: string) => {
   const chunkId = name.toLowerCase();
-  window.BLADE_CHUNKS[chunkId] = { [name]: component };
 
-  Object.defineProperties(component, {
-    $$typeof: { value: CLIENT_REFERENCE },
-    name: { value: name },
-    chunk: { value: chunkId },
-    id: { value: 'testing' },
-  });
+  if (typeof window === 'undefined') {
+    Object.defineProperties(component, {
+      $$typeof: { value: CLIENT_REFERENCE },
+      name: { value: name },
+      chunk: { value: chunkId },
+      id: { value: `native-${name}` },
+    });
+  } else {
+    window.BLADE_CHUNKS[chunkId] = { [name]: component };
+  }
 };
