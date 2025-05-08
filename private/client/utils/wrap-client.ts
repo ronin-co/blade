3c17f7668aac11e225640a7305e6de043de82508
Leo Lamprecht <leo@ronin.co>
2025-05-08 10:36:45 +0200

    Added TSDoc comment

--- private/client/utils/wrap-client.ts
+++ private/client/utils/wrap-client.ts
@@ -2,6 +2,14 @@ import type { ComponentType } from 'react';
 
 const CLIENT_REFERENCE = Symbol.for('react.client.reference');
 
+/**
+ * Used to mark internal framework components as client components.
+ *
+ * @param component - The React component to mark as a client component.
+ * @param name - The name of the React component.
+ *
+ * @returns Nothing.
+ */
 export const wrapClientComponent = (component: ComponentType<any>, name: string) => {
   const chunkId = name.toLowerCase();
 
