f11e9943f3d0ee2f51f5e677573d58ee7e43f6cd
Leo Lamprecht <leo@ronin.co>
2025-05-08 10:48:47 +0200

    Added code comment

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: [
+    // These files are publicly accessible.
     './public/client/hooks.ts',
     './public/client/components.tsx',
     './public/server/hooks.ts',
@@ -11,6 +12,7 @@ export default defineConfig({
     './public/universal/schema.ts',
     './public/universal/types.ts',
 
+    // These files are used internally by Blade.
     './private/shell/index.ts',
     './private/shell/builder.ts',
     './private/shell/listener.ts',
