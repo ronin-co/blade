698956383001d8c5c564c14c04665469f0b73aff
Leo Lamprecht <leo@ronin.co>
2025-05-30 21:08:31 +0200

    Ensure working `Image` component (#119)

--- private/client/utils/wrap-client.ts
+++ private/client/utils/wrap-client.ts
@@ -1,6 +1,10 @@
-import type { ComponentType } from 'react';
+import type { ComponentType, ExoticComponent, ForwardRefRenderFunction } from 'react';
 
 const CLIENT_REFERENCE = Symbol.for('react.client.reference');
+const REACT_FORWARD_REF_TYPE = Symbol.for('react.forward_ref');
+
+type Component = ComponentType<any> &
+  Partial<ExoticComponent & { render: ForwardRefRenderFunction<unknown, unknown> }>;
 
 /**
  * Used to mark internal framework components as client components.
@@ -10,16 +14,19 @@ const CLIENT_REFERENCE = Symbol.for('react.client.reference');
  *
  * @returns Nothing.
  */
-export const wrapClientComponent = (component: ComponentType<any>, name: string) => {
+export const wrapClientComponent = (component: Component, name: string) => {
   const chunkId = name.toLowerCase();
 
   if (typeof window === 'undefined') {
-    Object.defineProperties(component, {
-      $$typeof: { value: CLIENT_REFERENCE },
-      name: { value: name },
-      chunk: { value: chunkId },
-      id: { value: `native-${name}` },
-    });
+    Object.defineProperties(
+      component.$$typeof === REACT_FORWARD_REF_TYPE ? component.render : component,
+      {
+        $$typeof: { value: CLIENT_REFERENCE },
+        name: { value: name },
+        chunk: { value: chunkId },
+        id: { value: `native-${name}` },
+      },
+    );
   } else {
     window.BLADE_CHUNKS[chunkId] = { [name]: component };
   }
