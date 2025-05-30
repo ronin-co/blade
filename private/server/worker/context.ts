1f43c9716bcee0de9a01f4865e4d36caba172949
Leo Lamprecht <leo@ronin.co>
2025-05-30 15:17:46 +0200

    Simplify context (#113)

--- private/server/worker/context.ts
+++ private/server/worker/context.ts
@@ -1,15 +1,13 @@
-import type { AsyncLocalStorage } from 'node:async_hooks';
+import { AsyncLocalStorage } from 'node:async_hooks';
 
 import type { ServerContext } from '@/private/server/context';
 
-export const SERVER_CONTEXT = (
-  typeof window === 'undefined'
-    ? new (await import('node:async_hooks'))['AsyncLocalStorage']()
-    : null
-) as AsyncLocalStorage<ServerContext>;
+// The conditions below are used because the file is consumed by the universal context,
+// which means that it is also used in the browser, as part of the client context.
 
+export const SERVER_CONTEXT = (
+  AsyncLocalStorage ? new AsyncLocalStorage() : undefined
+) as AsyncLocalStorage<ServerContext>;
 export const REACT_CONTEXT = (
-  typeof window === 'undefined'
-    ? new (await import('node:async_hooks'))['AsyncLocalStorage']()
-    : null
+  AsyncLocalStorage ? new AsyncLocalStorage() : undefined
 ) as AsyncLocalStorage<object>;
