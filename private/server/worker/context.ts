007a9b843222c421a4e99bf8697d7ab619f0b5cd
Leo Lamprecht <leo@ronin.co>
2025-05-30 15:10:08 +0200

    Improved context

--- private/server/worker/context.ts
+++ private/server/worker/context.ts
@@ -2,5 +2,12 @@ import { AsyncLocalStorage } from 'node:async_hooks';
 
 import type { ServerContext } from '@/private/server/context';
 
-export const SERVER_CONTEXT = new AsyncLocalStorage<ServerContext>();
-export const REACT_CONTEXT = new AsyncLocalStorage<object>();
+// The conditions below are used because the file is consumed by the universal context,
+// which means that it is also used in the browser, as part of the client context.
+
+export const SERVER_CONTEXT = (
+  AsyncLocalStorage ? new AsyncLocalStorage() : undefined
+) as AsyncLocalStorage<ServerContext>;
+export const REACT_CONTEXT = (
+  AsyncLocalStorage ? new AsyncLocalStorage() : undefined
+) as AsyncLocalStorage<object>;
