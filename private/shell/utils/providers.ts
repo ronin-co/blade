f80ef0cb51cc155dee5f20e512045f3b5f412e92
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-02 12:53:16 +0100

    Added `nodejs_als` to `compatibility_flags` in Wrangler config

--- private/shell/utils/providers.ts
+++ private/shell/utils/providers.ts
@@ -153,7 +153,7 @@ export const transformToCloudflareOutput = async (): Promise<void> => {
             name: currentDirectoryName,
             main: '.blade/_worker.js',
             compatibility_date: '2025-06-02',
-            compatibility_flags: ['nodejs_compat'],
+            compatibility_flags: ['nodejs_als', 'nodejs_compat'],
             assets: {
               binding: 'ASSETS',
               directory: '.blade/',
