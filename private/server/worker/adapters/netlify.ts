8b606a61af57498233cb7c2c4f0182cd3d2b5fbf
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-03 12:15:28 +0100

    Added `config` export to `netlify` worker adapter

--- private/server/worker/adapters/netlify.ts
+++ private/server/worker/adapters/netlify.ts
@@ -2,4 +2,10 @@ import { handle } from 'hono/netlify';
 
 import app from '../index';
 
+import type { Config } from '@netlify/functions';
+
 export default handle(app);
+
+export const config = {
+  path: '/*',
+} satisfies Config;
