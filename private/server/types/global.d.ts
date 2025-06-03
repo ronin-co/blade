c9183a8af7a5b00e93bb608c91578a508715ac6a
Leo Lamprecht <leo@ronin.co>
2025-06-03 14:08:10 +0200

    Removed timezone mapping

--- private/server/types/global.d.ts
+++ private/server/types/global.d.ts
@@ -3,10 +3,3 @@ declare module 'server-list' {
   export const triggers: import('./index').TriggersList;
   export const router: import('hono').Hono | null;
 }
-
-declare module '@mapbox/timespace' {
-  export const getFuzzyLocalTimeFromPoint: (
-    time: number,
-    point: [number, number],
-  ) => null | { _z: { name: string } };
-}
