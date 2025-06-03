a62162f889351146c8fb2004cf534cb8241b33c9
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-03 17:31:20 +0100

    Temp: Log `typeof window` inside of `wrapClientComponent`

--- private/client/utils/wrap-client.ts
+++ private/client/utils/wrap-client.ts
@@ -17,6 +17,9 @@ type Component = ComponentType<any> &
 export const wrapClientComponent = (component: Component, name: string) => {
   const chunkId = name.toLowerCase();
 
+  console.log({
+    typeofWindow: typeof window,
+  });
   if (typeof window === 'undefined') {
     Object.defineProperties(
       component.$$typeof === REACT_FORWARD_REF_TYPE ? component.render : component,
