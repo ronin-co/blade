4b99f0e7e8e92b18d1359c811309ca35a4b77089
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-05 12:29:38 +0100

    Re-add `preferStatic` property to Netlify worker config

--- private/server/worker/adapters/netlify.ts
+++ private/server/worker/adapters/netlify.ts
@@ -2,10 +2,11 @@ import { handle } from 'hono/netlify';
 
 import app from '../index';
 
-import type { Config } from '@netlify/edge-functions';
+import type { Config } from '@netlify/functions';
 
 export default handle(app);
 
 export const config = {
   path: '/*',
+  preferStatic: true,
 } satisfies Config;
