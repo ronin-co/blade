2d752cbdb4ba69e69fab123ca38b0739ca8efde3
Leo Lamprecht <leo@ronin.co>
2025-06-05 13:39:19 +0200

    Bypass for client assets

--- private/server/providers/service-worker.ts
+++ private/server/providers/service-worker.ts
@@ -2,4 +2,11 @@ import { handle } from 'hono/service-worker';
 
 import app from '../worker/index';
 
-self.addEventListener('fetch', handle(app as any) as any);
+self.addEventListener('fetch', (event: FetchEvent) => {
+  const url = new URL(event.request.url);
+
+  // Ensure that requests targeting client-side assets are bypassing the service worker.
+  if (url.pathname.startsWith('/client')) return;
+
+  return handle(app as any)(event as any) as any;
+});
