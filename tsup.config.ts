0a651472805c9e0c93efab7804f1bc00afb0466c
Leo Lamprecht <leo@ronin.co>
2025-06-10 19:09:55 +0200

    Correctly export components (#164)

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: [
     // These files are publicly accessible.
-    './public/client/**/*.ts',
-    './public/server/**/*.ts',
-    './public/universal/**/*.ts',
+    './public/client/**/*.(ts|tsx)',
+    './public/server/**/*.(ts|tsx)',
+    './public/universal/**/*.(ts|tsx)',
 
     // These files are used internally by Blade.
     './private/shell/index.ts',
