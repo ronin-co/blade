b70e6ca4033519b9a97d0ed36ab8b07153efc0c0
Leo Lamprecht <leo@ronin.co>
2025-06-11 17:09:13 +0200

    Improved imports

--- public/universal/hooks.ts
+++ public/universal/hooks.ts
@@ -1,11 +1,11 @@
 import type { CookieSerializeOptions } from 'cookie';
+import { useContext } from 'react';
 
 import { type RootTransitionOptions, usePageTransition } from '@/private/client/hooks';
 import { RootServerContext } from '@/private/server/context';
 import { usePrivateLocation, useUniversalContext } from '@/private/universal/hooks';
 import type { CustomNavigator } from '@/private/universal/types/util';
 import { populatePathSegments } from '@/private/universal/utils/paths';
-import { useContext } from 'react';
 
 const useParams = <
   TParams extends Record<string, unknown> | Array<string> = Record<
