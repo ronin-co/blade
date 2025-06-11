d61cc5eaac6b873583b6360ee6bb0979771dae6e
Cornelius Denninger <cornelius.denninger@gmail.com>
2025-06-11 13:45:03 +0200

    Add section support (#170)

--- docs/pages/layout.tsx
+++ docs/pages/layout.tsx
@@ -6,98 +6,85 @@ import { OnThisPage } from '@/components/on-this-page.client';
 import { Sidebar } from '@/components/sidebar';
 import { useMetadata } from '@ronin/blade/server/hooks';
 
-const menuItems = [
-  {
-    id: 'introduction',
-    name: 'Introduction',
-    href: '/',
-    parentItem: '',
-  },
-  {
-    id: 'hooks',
-    name: 'Hooks',
-    href: '/hooks',
-    parentItem: '',
-  },
-  {
-    id: 'components',
-    name: 'Components',
-    href: '/components',
-    parentItem: '',
-  },
-  {
-    id: 'queries',
-    name: 'Queries',
-    href: '/queries',
-    parentItem: '',
-  },
-  {
-    id: 'crud',
-    name: 'CRUD',
-    href: '/queries/crud',
-    parentItem: 'queries',
-  },
-  {
-    id: 'instructions',
-    name: 'Instructions',
-    href: '/queries/instructions',
-    parentItem: 'queries',
-  },
-  {
-    id: 'functions',
-    name: 'Functions',
-    href: '/queries/functions',
-    parentItem: 'queries',
-  },
-  {
-    id: 'models',
-    name: 'Models',
-    href: '/models',
-    parentItem: '',
-  },
-  {
-    id: 'Fields',
-    name: 'Fields',
-    href: '/models/fields',
-    parentItem: 'models',
-  },
-  {
-    id: 'triggers',
-    name: 'Triggers',
-    href: '/models/triggers',
-    parentItem: 'models',
-  },
-  {
-    id: 'pages',
-    name: 'Pages',
-    href: '/pages',
-    parentItem: '',
-  },
-  {
-    id: 'api',
-    name: 'API Routes',
-    href: '/api-routes',
-    parentItem: '',
-  },
-  {
-    id: 'types',
-    name: 'Types',
-    href: '/types',
-    parentItem: '',
-  },
-  {
-    id: 'client',
-    name: 'Client',
-    href: '/client',
-    parentItem: '',
-  },
-  {
-    id: 'deploying',
-    name: 'Deploying',
-    href: '/deploying',
-    parentItem: '',
-  },
-];
+const menuItems = {
+  'Get Started': [
+    {
+      id: 'introduction',
+      name: 'Introduction',
+      href: '/',
+    },
+    {
+      id: 'hooks',
+      name: 'Hooks',
+      href: '/hooks',
+    },
+    {
+      id: 'components',
+      name: 'Components',
+      href: '/components',
+    },
+    {
+      id: 'queries',
+      name: 'Queries',
+      href: '/queries',
+    },
+    {
+      id: 'crud',
+      name: 'CRUD',
+      href: '/queries/crud',
+    },
+    {
+      id: 'instructions',
+      name: 'Instructions',
+      href: '/queries/instructions',
+    },
+    {
+      id: 'functions',
+      name: 'Functions',
+      href: '/queries/functions',
+    },
+    {
+      id: 'models',
+      name: 'Models',
+      href: '/models',
+    },
+    {
+      id: 'Fields',
+      name: 'Fields',
+      href: '/models/fields',
+    },
+    {
+      id: 'triggers',
+      name: 'Triggers',
+      href: '/models/triggers',
+    },
+    {
+      id: 'pages',
+      name: 'Pages',
+      href: '/pages',
+    },
+    {
+      id: 'api',
+      name: 'API Routes',
+      href: '/api-routes',
+    },
+    {
+      id: 'types',
+      name: 'Types',
+      href: '/types',
+    },
+    {
+      id: 'client',
+      name: 'Client',
+      href: '/client',
+    },
+    {
+      id: 'deploying',
+      name: 'Deploying',
+      href: '/deploying',
+    },
+  ],
+};
 
 interface HeadingProps {
   children: React.ReactNode;
@@ -115,10 +102,7 @@ const DocsLayout = ({
     <div className="relative z-10 flex min-h-svh w-full flex-col bg-background">
       <Navbar />
       <div className="fixed top-24 left-6 w-fit">
-        <Sidebar
-          title="Get Started"
-          items={menuItems}
-        />
+        <Sidebar items={menuItems} />
       </div>
       <div className="docs-content prose mx-auto flex w-full min-w-0 max-w-2xl flex-1 flex-col px-4 py-6 text-neutral-800 md:px-0 lg:py-8 dark:text-neutral-300">
         {children}
