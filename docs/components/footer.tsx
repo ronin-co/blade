b9e1e6fe2bee7875b3a90c4d3b7028d7e2d56e83
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-12 09:36:22 +0100

    Fixed footer max width

--- docs/components/footer.tsx
+++ docs/components/footer.tsx
@@ -2,7 +2,7 @@ import { Icons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
 
 export const Footer = () => (
-  <footer className="mx-auto w-full max-w-2xl space-y-10 pb-16 lg:max-w-5xl">
+  <footer className="mx-auto w-full max-w-2xl space-y-10 pb-16">
     <div className="flex flex-col items-center justify-between gap-5 border-zinc-900/5 border-t pt-8 sm:flex-row dark:border-white/5">
       <p className="text-xs text-zinc-600 dark:text-zinc-400">
         &copy; Copyright {new Date().getFullYear()}. All rights reserved.
