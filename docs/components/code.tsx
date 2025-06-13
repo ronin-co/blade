b5d75ce474a589f5c7d57dc7c0024afbe8bed7c4
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-13 11:02:19 +0100

    Minor syntax highlighting fixes & tweaks

--- docs/components/code.tsx
+++ docs/components/code.tsx
@@ -2,6 +2,79 @@ import { highlight } from 'sugar-high';
 
 import { CopyToClipboard } from '@/components/copy.client';
 
+/**
+ * This Set is a fork of the official JavaScript keywords preset.
+ * The reason for this is that we are adding TypeScript keywords to the set.
+ * @see https://github.com/huozhi/sugar-high/blob/main/lib/index.js#L4-L46
+ */
+const TypeScriptKeywords = new Set([
+  'abstract',
+  'as',
+  'asserts',
+  'async',
+  'await',
+  'break',
+  'case',
+  'catch',
+  'class',
+  'const',
+  'continue',
+  'debugger',
+  'declare',
+  'default',
+  'delete',
+  'do',
+  'else',
+  'enum',
+  'export',
+  'extends',
+  'false',
+  'finally',
+  'for',
+  'from',
+  'function',
+  'if',
+  'implements',
+  'import',
+  'in',
+  'infer',
+  'instanceof',
+  'interface',
+  'is',
+  'keyof',
+  'let',
+  'module',
+  'namespace',
+  'new',
+  'readonly',
+  'return',
+  'satisfies',
+  'static',
+  'super',
+  'switch',
+  'this',
+  'throw',
+  'true',
+  'try',
+  'type',
+  'typeof',
+  'undefined',
+  'var',
+  'void',
+  'while',
+  'with',
+  'yield',
+]);
+
+type SupportedLanguages = 'bash' | 'ts' | 'tsx' | 'typescript';
+
+const MAPPED_LANGUAGE_KEYWORDS = {
+  bash: new Set<string>([]),
+  ts: TypeScriptKeywords,
+  tsx: TypeScriptKeywords,
+  typescript: TypeScriptKeywords,
+} satisfies Record<SupportedLanguages, Set<string>>;
+
 interface CodeBlockProps extends React.ComponentPropsWithoutRef<'pre'> {
   language?: string;
 }
@@ -10,6 +83,11 @@ export const CodeBlock = (props: CodeBlockProps) => {
   const { language, children: defaultChildren, ...restProps } = props;
   const children = Array.isArray(defaultChildren) ? defaultChildren[0] : defaultChildren;
 
+  const keywords = MAPPED_LANGUAGE_KEYWORDS[language as SupportedLanguages];
+  const html = highlight(children, {
+    keywords: keywords ?? new Set<string>(),
+  });
+
   return (
     <div className="relative">
       <pre
@@ -17,7 +95,7 @@ export const CodeBlock = (props: CodeBlockProps) => {
         className="not-prose relative my-0! overflow-x-auto whitespace-pre-wrap rounded-lg border p-5 text-sm">
         <code
           className="bg-none"
-          dangerouslySetInnerHTML={{ __html: highlight(children) }}
+          dangerouslySetInnerHTML={{ __html: html }}
         />
         <div className="absolute top-3 right-2">
           <CopyToClipboard content={children as string} />
