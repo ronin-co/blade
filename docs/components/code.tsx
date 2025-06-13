88182854f0c40d44d2990112a4f4e7df297d5d0c
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-13 11:20:06 +0100

    Add syntax highlighting to documentation (#192)

--- docs/components/code.tsx
+++ docs/components/code.tsx
@@ -1,4 +1,78 @@
+import { highlight } from 'sugar-high';
+
 import { CopyToClipboard } from '@/components/copy.client';
+
+/**
+ * This Set is a fork of the official JavaScript keywords preset.
+ * The reason for this is that we are adding TypeScript keywords to the set.
+ * @see https://github.com/huozhi/sugar-high/blob/main/lib/index.js#L4-L46
+ */
+const TypeScriptKeywords = new Set([
+  'abstract',
+  'as',
+  'asserts',
+  'async',
+  'await',
+  'break',
+  'case',
+  'catch',
+  'class',
+  'const',
+  'continue',
+  'debugger',
+  'declare',
+  'default',
+  'delete',
+  'do',
+  'else',
+  'enum',
+  'export',
+  'extends',
+  'false',
+  'finally',
+  'for',
+  'from',
+  'function',
+  'if',
+  'implements',
+  'import',
+  'in',
+  'infer',
+  'instanceof',
+  'interface',
+  'is',
+  'keyof',
+  'let',
+  'module',
+  'namespace',
+  'new',
+  'readonly',
+  'return',
+  'satisfies',
+  'static',
+  'super',
+  'switch',
+  'this',
+  'throw',
+  'true',
+  'try',
+  'type',
+  'typeof',
+  'undefined',
+  'var',
+  'void',
+  'while',
+  'with',
+  'yield',
+]);
+
+type SupportedLanguages = 'ts' | 'tsx' | 'typescript';
+
+const MAPPED_LANGUAGE_KEYWORDS = {
+  ts: TypeScriptKeywords,
+  tsx: TypeScriptKeywords,
+  typescript: TypeScriptKeywords,
+} satisfies Record<SupportedLanguages, Set<string>>;
 
 interface CodeBlockProps extends React.ComponentPropsWithoutRef<'pre'> {
   language?: string;
@@ -8,12 +82,20 @@ export const CodeBlock = (props: CodeBlockProps) => {
   const { language, children: defaultChildren, ...restProps } = props;
   const children = Array.isArray(defaultChildren) ? defaultChildren[0] : defaultChildren;
 
+  const keywords = MAPPED_LANGUAGE_KEYWORDS[language as SupportedLanguages];
+  const html = highlight(children, {
+    keywords: keywords ?? new Set<string>(),
+  });
+
   return (
     <div className="relative">
       <pre
         {...restProps}
-        className="not-prose relative my-0! overflow-x-auto whitespace-pre-wrap rounded-lg border p-5 text-sm">
-        <code className="bg-none">{children}</code>
+        className="not-prose relative my-0! overflow-x-auto rounded-lg border p-5 text-sm">
+        <code
+          className="bg-none"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
         <div className="absolute top-3 right-2">
           <CopyToClipboard content={children as string} />
         </div>
