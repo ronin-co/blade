6c864bc4b9ec4c30f39bbf65e021e94a729ba8be
Leo Lamprecht <leo@ronin.co>
2025-06-16 19:36:40 +0200

    Use less code

--- private/client/utils/parser.ts
+++ private/client/utils/parser.ts
@@ -104,13 +104,11 @@ const resolveModel = (response: ChunkResponse, id: number, model: string) => {
   if (chunk) {
     chunks.set(id, model);
 
-    const resolveListeners = chunk.value;
+    if (chunk !== null) {
+      const value = parseModel(response, model);
 
-    if (resolveListeners !== null) {
-      const value = parseModel(chunk._response, model);
-
-      for (let i = 0; i < resolveListeners.length; i++) {
-        const listener = resolveListeners[i];
+      for (let i = 0; i < chunk.length; i++) {
+        const listener = chunk[i];
         listener(value);
       }
     }
