81c5bc1bc9b93abfe5eb713a46ca5524efc569c6
Cornelius <cornelius.denninger@gmail.com>
2025-06-17 13:21:44 +0200

    Remove console.log

--- private/server/worker/tree.ts
+++ private/server/worker/tree.ts
@@ -346,7 +346,6 @@ const getRenderingTree = (
   let toc: Toc = [];
 
   for (const [path, leaf] of leaves.entries()) {
-    console.log(leaf.tableOfContents);
     toc = toc.concat(leaf.tableOfContents || []);
     if (!path.includes('layout.')) continue;
     components = { ...components, ...leaf.components };
