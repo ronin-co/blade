c66d5821b10c4f5ff312cfa3a8bf67f98c6e0c3d
Leo Lamprecht <leo@ronin.co>
2025-06-22 14:42:11 +0200

    Ensure correct hash links in docs (#240)

--- docs/components/toc-item.client.tsx
+++ docs/components/toc-item.client.tsx
@@ -1,4 +1,5 @@
 import type { TableOfContents } from '@ronin/blade/types';
+import type { MouseEventHandler } from 'react';
 
 import { slugify } from '@/lib/utils';
 
@@ -9,9 +10,18 @@ export const TableOfContentsItem = ({
   heading: TableOfContents[number];
   activeId: string;
 }) => {
-  const handleHeadingClick = (id: string) => {
-    const element = document.getElementById(id);
+  const hash = slugify(heading.value);
 
+  const handleHeadingClick: MouseEventHandler = (event) => {
+    // Prevent the browser from using the default scrolling effect.
+    event.preventDefault();
+
+    // Attach the hash to the current URL.
+    document.location.hash = hash;
+
+    const element = document.getElementById(hash);
+
+    // Smoothly scroll to the element instead of scrolling abruptly.
     if (element) {
       element.scrollIntoView({
         behavior: 'smooth',
@@ -27,13 +37,15 @@ export const TableOfContentsItem = ({
       data-slot="sidebar-menu-item"
       data-sidebar="menu-item"
       className="group/menu-item relative">
-      <button
+      <a
+        // The link must work even before the JS is downloaded.
+        href={`#${hash}`}
         type="button"
         data-slot="sidebar-menu-button"
         data-sidebar="menu-button"
         data-size="default"
         data-active={activeId === heading.value ? 'true' : 'false'}
-        onClick={() => heading.value && handleHeadingClick(slugify(heading.value))}
+        onClick={handleHeadingClick}
         className={`peer/menu-button ${
           activeId === slugify(heading.value) ? 'text-primary' : 'text-muted-foreground'
         } after:-inset-y-1 relative flex h-[30px] 3xl:fixed:w-full w-fit 3xl:fixed:max-w-48 items-center gap-2 overflow-visible rounded-md p-2 text-left font-medium text-[0.8rem] outline-hidden ring-sidebar-ring transition-[width,height,padding,color] after:absolute after:inset-x-0 after:z-0 after:rounded-md hover:text-primary focus-visible:ring-2 active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 group-has-data-[sidebar=menu-action]/menu-item:pr-8 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:border-accent data-[active=true]:font-medium data-[active=true]:text-sidebar-accent-foreground data-[state=open]:hover:bg-sidebar-accent data-[state=open]:hover:text-sidebar-accent-foreground group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0`}
@@ -41,7 +53,7 @@ export const TableOfContentsItem = ({
           paddingLeft: `${0.5 + (heading.depth - 1) * 0.5}rem`,
         }}>
         <span className="truncate">{heading.value}</span>
-      </button>
+      </a>
       {heading.children && heading.children.length > 0 && (
         <ul className="flex w-full min-w-0 flex-col gap-0.5">
           {heading.children.map((child) => (
