bb6c418975268cb5e56cb873935c5b3d0f74033e
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-23 16:43:26 +0100

    Added initial `parseArgs` structure for `create-blade`

--- packages/create-blade/src/index.ts
+++ packages/create-blade/src/index.ts
@@ -1,1 +1,20 @@
-export {};
+#!/usr/bin/env node
+
+import { parseArgs } from 'node:util';
+
+async function main(): Promise<void> {
+  await new Promise((resolve) => setTimeout(resolve, 0));
+
+  const { positionals, values } = parseArgs({
+    allowPositionals: true,
+    args: process.argv,
+    options: {},
+    strict: true,
+  });
+  console.log({
+    positionals,
+    values,
+  });
+}
+
+main().catch(console.error);
