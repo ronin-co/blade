f3a6fd8f2afe1ef066cc1c476caae1ec11f72b14
Juri Adams <juri@adams.sh>
2025-06-23 20:58:24 +0200

    Fixed footer not sizing properly on all viewports

--- docs/components/footer.tsx
+++ docs/components/footer.tsx
@@ -21,7 +21,9 @@ export const Footer = (props: FooterProps) => {
   const pathname = getEditUrl(location);
 
   return (
-    <footer className="sticky top-0 z-50 flex w-full items-center justify-around border-border border-t bg-background px-6 py-6">
+    <footer className="flex w-full items-start justify-center gap-x-12 border-border border-t bg-background px-8 py-6 lg:px-6">
+      <div className="hidden w-48 shrink-0 md:block lg:w-64" />
+
       <div className="flex w-full max-w-3xl flex-col justify-between gap-2 sm:flex-row sm:items-center 2xl:max-w-4xl">
         <p className="flex flex-row items-center gap-1 font-medium text-muted-foreground text-xs">
           <a
@@ -62,6 +64,8 @@ export const Footer = (props: FooterProps) => {
           </a>
         </div>
       </div>
+
+      <div className="hidden w-48 shrink-0 lg:w-64 xl:block" />
     </footer>
   );
 };
