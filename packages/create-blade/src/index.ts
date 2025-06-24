369c4b90f1081c9bf66cd1f9d97c96be5e3c509a
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-24 11:11:31 +0100

    Added more WIP `create-blade` CLI logic

--- packages/create-blade/src/index.ts
+++ packages/create-blade/src/index.ts
@@ -2,19 +2,64 @@
 
 import { parseArgs } from 'node:util';
 
+import pkg from '@/package.json';
+
+import { logSpinner, loggingPrefixes } from '@/utils/log';
+
+const TEMPLATES = ['advanced', 'basic'] as const;
+
+type Template = (typeof TEMPLATES)[number];
+
 async function main(): Promise<void> {
   await new Promise((resolve) => setTimeout(resolve, 0));
 
-  const { positionals, values } = parseArgs({
+  const { values } = parseArgs({
     allowPositionals: true,
     args: process.argv,
-    options: {},
+    options: {
+      help: {
+        default: false,
+        short: 'h',
+        type: 'boolean',
+      },
+      template: {
+        default: 'basic' satisfies Template,
+        short: 'T',
+        type: 'string',
+      },
+      version: {
+        default: false,
+        short: 'v',
+        type: 'boolean',
+      },
+    },
     strict: true,
   });
-  console.log({
-    positionals,
-    values,
-  });
+
+  if (values.version) {
+    console.log(pkg.version);
+    process.exit(0);
+  }
+
+  if (values.help) {
+    console.log(`
+Usage: create-blade [directory] [options]
+
+Options:
+  -v, --version           Output the current version of create-blade.
+  -h, --help              Display this help message.
+`);
+    process.exit(0);
+  }
+
+  if (!TEMPLATES.includes(values.template as Template)) {
+    console.error(loggingPrefixes.error, `Invalid template "${values.template}"`);
+    console.error(loggingPrefixes.error, 'Available templates:', TEMPLATES.join(', '));
+    process.exit(1);
+  }
+
+  const spinner = logSpinner('Creating a new Blade project...').start();
+  spinner.succeed();
 }
 
 main().catch(console.error);
---
+++ packages/create-blade/src/utils/log.ts
@@ -0,0 +1,19 @@
+import chalk from 'chalk';
+import ora from 'ora';
+
+import { gradient } from '@/utils/string';
+
+import type { Ora } from 'ora';
+
+export const loggingPrefixes = {
+  info: `${chalk.bold(gradient(['#473b7b', '#3584a7', '#30d2be'])('BLADE'))} `,
+  error: `${chalk.bold(gradient(['#930024', '#d4143e'])('ERROR'))}  `,
+};
+
+export const logSpinner = (text: string): Ora =>
+  ora({
+    // Make CTRL+C work as expected.
+    discardStdin: false,
+    prefixText: loggingPrefixes.info,
+    text,
+  });
