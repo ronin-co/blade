84364173cd03630f309ce0fbf7651c1b1b38730b
Leo Lamprecht <leo@ronin.co>
2025-06-25 19:40:42 +0200

    Add support for Deno

--- packages/blade/tsup.config.ts
+++ packages/blade/tsup.config.ts
@@ -18,4 +18,38 @@ export default defineConfig({
   external: ['server-list', 'client-list', 'build-meta', 'react', 'react-dom'],
   publicDir: './private/client/assets',
   treeshake: true,
+  esbuildPlugins: [
+    // Deno requires Node.js primitives to be imported with the `node:` prefix
+    // and certain dependencies that Blade uses don't do that, so we're aliasing
+    // them ourselves.
+    {
+      name: 'alias-native-modules',
+      setup(build) {
+        const items = [
+          'stream',
+          'crypto',
+          'http2',
+          'http',
+          'os',
+          'events',
+          'path',
+          'fs',
+          'fs/promises',
+        ];
+
+        for (const item of items) {
+          build.onResolve({ filter: new RegExp(`^${item}$`) }, () => ({
+            path: `node:${item}`,
+            external: true,
+            namespace: 'node-builtin',
+          }));
+        }
+      },
+    },
+  ],
+  define: {
+    // Deno does not support `global` and certain dependencies that Blade uses are
+    // using `global`, so we're aliasing it ourselves.
+    global: 'globalThis'
+  }
 });
