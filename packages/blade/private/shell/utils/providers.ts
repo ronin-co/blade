9c0ada802cdea2d0afd6ebae8c4cf96199afd9f8
Ben <4991309+NuroDev@users.noreply.github.com>
2025-06-25 18:28:21 +0100

    Updated Vercel `config.json`

--- packages/blade/private/shell/utils/providers.ts
+++ packages/blade/private/shell/utils/providers.ts
@@ -66,13 +66,13 @@ export const transformToVercelBuildOutput = async (): Promise<void> => {
         routes: [
           {
             handle: 'filesystem',
+          },
+          {
+            src: '^/(.*)$',
             headers: {
               'cache-control': 'public, max-age=31536000, immutable',
             },
-          },
-          {
-            src: '^(?:/(.*?))?/?$',
-            dest: 'worker',
+            continue: true,
           },
         ],
       } satisfies VercelConfig),
