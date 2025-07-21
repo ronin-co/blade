09e36152093e1c0add6c1dd3ce3498194c9cbcf2
Ben <4991309+NuroDev@users.noreply.github.com>
2025-07-21 16:49:51 +0100

    Temp: Switch `createSyntaxFactory` back to `ronin`

--- packages/blade/private/server/types/index.ts
+++ packages/blade/private/server/types/index.ts
@@ -1,6 +1,6 @@
 import type { Toc } from '@stefanprobst/rehype-extract-toc';
-import type { createSyntaxFactory } from 'blade-client';
 import type { ComponentType, FunctionComponent } from 'react';
+import type { createSyntaxFactory } from 'ronin';
 import type {
   AddTrigger as OriginalAddTrigger,
   AfterAddTrigger as OriginalAfterAddTrigger,
