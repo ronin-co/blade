e738ac893ef0323a32b77aa64aa5249f1f962d71
nurodev <ben@nuro.dev>
2025-08-06 14:15:26 +0100

    Move Node.js import aliases to top-level

--- packages/blade/build.config.ts
+++ packages/blade/build.config.ts
@@ -5,6 +5,19 @@ export default defineBuildConfig({
   alias: {
     '@/private': resolve(__dirname, 'private'),
     '@/public': resolve(__dirname, 'public'),
+
+    // Deno requires Node.js primitives to be imported with the `node:` prefix
+    // and certain dependencies that Blade uses don't do that, so we're aliasing
+    // them ourselves.
+    'fs/promises': 'node:fs/promises',
+    crypto: 'node:crypto',
+    events: 'node:events',
+    fs: 'node:fs',
+    http: 'node:http',
+    http2: 'node:http2',
+    os: 'node:os',
+    path: 'node:path',
+    stream: 'node:stream',
   },
   entries: [
     // These files are publicly accessible.
@@ -33,20 +46,6 @@ export default defineBuildConfig({
   failOnWarn: false,
   parallel: true,
   rollup: {
-    alias: {
-      // Deno requires Node.js primitives to be imported with the `node:` prefix
-      // and certain dependencies that Blade uses don't do that, so we're aliasing
-      // them ourselves.
-      'fs/promises': 'node:fs/promises',
-      crypto: 'node:crypto',
-      events: 'node:events',
-      fs: 'node:fs',
-      http: 'node:http',
-      http2: 'node:http2',
-      os: 'node:os',
-      path: 'node:path',
-      stream: 'node:stream',
-    },
     emitCJS: false,
     esbuild: {
       define: {
@@ -54,6 +53,7 @@ export default defineBuildConfig({
         // using `global`, so we're aliasing it ourselves.
         global: 'globalThis',
       },
+      // Add the `React` global for JSX support
       jsx: 'automatic',
     },
     output: {
