8504222f931010a3a97ca3f106d6e32cba3ce200
Leo Lamprecht <leo@ronin.co>
2025-08-06 12:47:49 +0200

    Ensure correct types

--- packages/blade/public/universal/utils.ts
+++ packages/blade/public/universal/utils.ts
@@ -1,13 +1,18 @@
-import { build as buildContext } from '@/private/shell/utils/build';
 import type { BuildOptions, BuildResult } from 'esbuild';
+
+import { build as buildContext } from '@/private/shell/utils/build';
 
 interface BuildConfig {
   environment?: 'development' | 'production';
 }
 
-export const build = async (config?: BuildConfig): Promise<BuildResult<BuildOptions>> => {
+interface BuildOutput
+  extends Pick<BuildResult<BuildOptions>, 'errors' | 'warnings' | 'outputFiles'> {}
+
+export const build = async (config?: BuildConfig): Promise<BuildOutput> => {
   const environment = config?.environment || 'development';
   const mainBuild = await buildContext(environment);
+  const { errors, warnings, outputFiles } = await mainBuild.rebuild();
 
-  return mainBuild.rebuild();
+  return { errors, warnings, outputFiles };
 };
