7eec604ca0e1f6d7605f7f84210220b698ecb1c1
Leo Lamprecht <leo@ronin.co>
2025-08-14 14:12:27 +0200

    Ensure correct Vercel deployments

--- packages/blade/private/shell/utils/build.ts
+++ packages/blade/private/shell/utils/build.ts
@@ -117,7 +117,7 @@ export const composeBuildContext = async (
         dir: outputDirectory,
         sourcemap: true,
         banner: `if(!import.meta.env){import.meta.env={}};import.meta.env.__BLADE_BUNDLE_ID='${bundleId}';`,
-        minify: environment === 'production',
+        minify: false,
         entryFileNames: (chunk) => {
           if (chunk.name === 'client') return getOutputFile(bundleId, 'js');
           return '[name].js';
