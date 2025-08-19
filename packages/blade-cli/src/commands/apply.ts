183d3c0aabecad35851f557267ad950e820d092d
nurodev <ben@nuro.dev>
2025-08-19 09:44:00 +0100

    Minor code cleanup

--- packages/blade-cli/src/commands/apply.ts
+++ packages/blade-cli/src/commands/apply.ts
@@ -3,7 +3,6 @@ import path from 'node:path';
 import { select } from '@inquirer/prompts';
 import { CompilerError } from 'blade-compiler';
 
-// import types from '@/src/commands/types';
 import type { MigrationFlags } from '@/src/utils/migration';
 import { MIGRATIONS_PATH } from '@/src/utils/misc';
 import {
@@ -79,9 +78,11 @@ export default async (
 
     spinner.succeed('Successfully applied migration');
 
-    // TODO(@nurodev): Re-enable once the `types` command dynamic import is fixed.
-    // // If desired, generate new TypeScript types.
-    // if (!flags['skip-types']) await types(appToken, sessionToken);
+    // If desired, generate new TypeScript types.
+    if (!flags['skip-types']) {
+      // TODO(@nurodev): Re-enable once the `types` command dynamic import is fixed.
+      // await types(appToken, sessionToken);
+    }
 
     process.exit(0);
   } catch (err) {
