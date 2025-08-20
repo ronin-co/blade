1947e691ade35c65287b023d8b19b9fd3bf2c272
nurodev <ben@nuro.dev>
2025-08-20 18:03:43 +0100

    Re-enable post-apply command type generation

--- packages/blade-cli/src/commands/apply.ts
+++ packages/blade-cli/src/commands/apply.ts
@@ -3,6 +3,7 @@ import path from 'node:path';
 import { select } from '@inquirer/prompts';
 import { CompilerError } from 'blade-compiler';
 
+import types from '@/src/commands/types';
 import type { MigrationFlags } from '@/src/utils/migration';
 import { MIGRATIONS_PATH } from '@/src/utils/misc';
 import {
@@ -79,10 +80,7 @@ export default async (
     spinner.succeed('Successfully applied migration');
 
     // If desired, generate new TypeScript types.
-    if (!flags['skip-types']) {
-      // TODO(@nurodev): Re-enable once the `types` command dynamic import is fixed.
-      // await types(appToken, sessionToken);
-    }
+    if (!flags['skip-types']) await types(appToken, sessionToken);
 
     process.exit(0);
   } catch (err) {
