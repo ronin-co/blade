affca9aedd076c1f683cd80d9157f8057e6bc4d5
Leo Lamprecht <leo@ronin.co>
2025-08-25 17:20:47 +0200

    Decrease size of client-side bundles (#418)

--- packages/blade-client/src/utils/index.ts
+++ packages/blade-client/src/utils/index.ts
@@ -1,3 +1,2 @@
-export { runQueriesWithStorageAndTriggers as runQueries } from '@/src/queries';
 export { processStorableObjects, isStorableObject } from '@/src/storage';
 export { ClientError } from '@/src/utils/errors';
