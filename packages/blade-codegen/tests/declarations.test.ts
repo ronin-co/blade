e9c5c6351ebe5b1236c7abcd6ab189b4049fd151
nurodev <ben@nuro.dev>
2025-09-01 17:36:43 +0100

    Minor test tweaks

--- packages/blade-codegen/tests/declarations.test.ts
+++ packages/blade-codegen/tests/declarations.test.ts
@@ -1,31 +1,92 @@
 import { describe, expect, test } from 'bun:test';
 
 import {
-  importRoninQueryTypesType,
-  importRoninStoredObjectType,
-  importSyntaxUtiltypesType,
+  importBladeCompilerQueryTypesType,
+  importBladeCompilerStoredObjectType,
+  importSyntaxUtilTypesType,
+  jsonArrayType,
+  jsonObjectType,
+  jsonPrimitiveType,
+  reducedFunctionType,
+  resolveSchemaType,
 } from '@/src/declarations';
 import { printNodes } from '@/src/utils/print';
 
 describe('declarations', () => {
-  test('import the RONIN namespace type from `ronin`', () => {
-    const output = printNodes([importRoninQueryTypesType]);
+  test('import the query types from `blade-compiler`', () => {
+    const output = printNodes([importBladeCompilerQueryTypesType]);
     expect(output).toStrictEqual(
-      `import type { AddQuery, CountQuery, GetQuery, ListQuery, Model, RemoveQuery, SetQuery } from \"blade-compiler\";\n`,
+      `import type { AddQuery, CountQuery, GetQuery, RemoveQuery, SetQuery } from \"blade-compiler\";\n`,
     );
   });
 
-  test('import the RONIN namespace type from `ronin`', () => {
-    const output = printNodes([importRoninStoredObjectType]);
+  test('import the stored object from `blade-compiler`', () => {
+    const output = printNodes([importBladeCompilerStoredObjectType]);
     expect(output).toStrictEqual(
       `import type { StoredObject } from \"blade-compiler\";\n`,
     );
   });
 
-  test('import the RONIN namespace type from `ronin`', () => {
-    const output = printNodes([importSyntaxUtiltypesType]);
+  test('import the utility query types from `blade-syntax/queries`', () => {
+    const output = printNodes([importSyntaxUtilTypesType]);
     expect(output).toStrictEqual(
-      `import type { DeepCallable, ResultRecord } from \"blade-syntax/queries\";\n`,
+      `import type { ResultRecord } from \"blade-syntax/queries\";\n`,
     );
+  });
+
+  test('create `ResolveSchema` utility type`', () => {
+    const output = printNodes([resolveSchemaType]);
+    expect(output).toStrictEqual(
+      `type ResolveSchema<S, U extends Array<string> | "all", K extends string> = U extends "all" ? S : K extends U[number] ? S : S extends Array<unknown> ? Array<string> : string;\n`,
+    );
+  });
+
+  test('create `JsonPrimitive` utility type', () => {
+    const output = printNodes([jsonPrimitiveType]);
+    expect(output).toStrictEqual(
+      'type JsonPrimitive = string | number | boolean | null;\n',
+    );
+  });
+
+  test('create `JsonObject` utility type', () => {
+    const output = printNodes([jsonObjectType]);
+    expect(output).toStrictEqual(
+      `type JsonObject = {
+    [key: string]: JsonPrimitive | JsonObject | JsonArray;
+};\n`,
+    );
+  });
+
+  test('create `JsonArray` utility type', () => {
+    const output = printNodes([jsonArrayType]);
+    expect(output).toStrictEqual(
+      'type JsonArray = Array<JsonPrimitive | JsonObject | JsonArray>;\n',
+    );
+  });
+
+  test('create `ReducedFunction` utility interface', () => {
+    const output = printNodes([reducedFunctionType]);
+    expect(output).toStrictEqual(`interface ReducedFunction {
+    /* @deprecated */
+    apply: string;
+    /* @deprecated */
+    arguments: string;
+    /* @deprecated */
+    bind: string;
+    /* @deprecated */
+    call: string;
+    /* @deprecated */
+    caller: string;
+    /* @deprecated */
+    length: string;
+    /* @deprecated */
+    name: string;
+    /* @deprecated */
+    prototype: string;
+    /* @deprecated */
+    toString: string;
+    /* @deprecated */
+    unify: string;
+}\n`);
   });
 });
