574804880bcc5af1d06aaa88179f574d8a573688
Leo Lamprecht <leo@ronin.co>
2025-09-01 09:14:00 +0200

    Removed flag

--- packages/blade/private/shell/utils/providers.ts
+++ packages/blade/private/shell/utils/providers.ts
@@ -165,7 +165,6 @@ export const transformToCloudflareOutput = async (): Promise<void> => {
             name: currentDirectoryName,
             main: '.blade/edge-worker.js',
             compatibility_date: '2025-06-02',
-            compatibility_flags: ['nodejs_compat'],
             assets: {
               binding: 'ASSETS',
               directory: '.blade/',
