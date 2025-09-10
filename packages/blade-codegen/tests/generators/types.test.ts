3e6601d5186840d324056b5c7c7151a1fdf35b82
nurodev <ben@nuro.dev>
2025-09-10 12:00:54 +0100

    Added syntax types unit tests

--- packages/blade-codegen/tests/generators/types.test.ts
+++ packages/blade-codegen/tests/generators/types.test.ts
@@ -9,9 +9,20 @@ import {
   number,
   string,
 } from 'blade-syntax/schema';
+import { factory } from 'typescript';
 
 import { generateModelTypes } from '@/src/generators/types/model';
+import {
+  generateDefaultSyntaxProperty,
+  generateOrderedBySyntaxProperty,
+  generateRootQueryCallSignature,
+  generateSelectingSyntaxProperty,
+  generateUsingSyntaxProperty,
+  generateWithSyntaxProperty,
+} from '@/src/generators/types/syntax';
 import { printNodes } from '@/src/utils/print';
+
+import type { Model } from '@/src/types/model';
 
 describe('types', () => {
   describe('generate the core model types', () => {
@@ -193,6 +204,172 @@ describe('types', () => {
       const typesResultStr = printNodes(typesResult);
 
       expect(typesResultStr).toMatchSnapshot();
+    });
+  });
+
+  describe('syntax', () => {
+    describe('generate root query call signature', () => {
+      test('a basic model', () => {
+        const typesResult = generateRootQueryCallSignature(
+          factory.createTypeReferenceNode('Account'),
+        );
+
+        const typesResultStr = printNodes([typesResult]);
+
+        expect(typesResultStr).toMatchSnapshot();
+      });
+    });
+    describe('generate `default` syntax property', () => {
+      test('a basic model', () => {
+        const typesResult = generateDefaultSyntaxProperty(
+          'including',
+          factory.createTypeReferenceNode('Account'),
+        );
+
+        const typesResultStr = printNodes([typesResult]);
+
+        expect(typesResultStr).toMatchSnapshot();
+      });
+    });
+    describe('generate `orderedBy` syntax property', () => {
+      test('a basic model', () => {
+        const AccountModel = model({
+          slug: 'account',
+          pluralSlug: 'accounts',
+          fields: {
+            avatar: blob(),
+            email: string({ required: true }),
+            isActive: boolean(),
+            lastActiveAt: date(),
+            name: string(),
+            rewardPoints: number({ defaultValue: 0, required: true }),
+            settings: json({ defaultValue: {}, required: true }),
+          },
+        }) as unknown as Model;
+
+        const typesResult = generateOrderedBySyntaxProperty(
+          AccountModel,
+          factory.createTypeReferenceNode(AccountModel.slug),
+        );
+
+        const typesResultStr = printNodes([typesResult]);
+
+        expect(typesResultStr).toMatchSnapshot();
+      });
+    });
+    describe('generate `selecting` syntax property', () => {
+      test('a basic model', () => {
+        const AccountModel = model({
+          slug: 'account',
+          pluralSlug: 'accounts',
+          fields: {
+            avatar: blob(),
+            email: string({ required: true }),
+            isActive: boolean(),
+            lastActiveAt: date(),
+            name: string(),
+            rewardPoints: number({ defaultValue: 0, required: true }),
+            settings: json({ defaultValue: {}, required: true }),
+          },
+        }) as unknown as Model;
+
+        const typesResult = generateSelectingSyntaxProperty(
+          AccountModel,
+          factory.createTypeReferenceNode(AccountModel.slug),
+        );
+
+        const typesResultStr = printNodes([typesResult]);
+
+        expect(typesResultStr).toMatchSnapshot();
+      });
+    });
+    describe('generate `using` syntax property', () => {
+      test('a basic model', () => {
+        const PostModel = model({
+          slug: 'post',
+          pluralSlug: 'posts',
+          fields: {
+            title: string(),
+            author: link({ target: 'account' }),
+          },
+        }) as unknown as Model;
+
+        const singularTypesResult = generateUsingSyntaxProperty(
+          PostModel,
+          factory.createTypeReferenceNode('Post'),
+          PostModel.slug,
+          false,
+        );
+        const pluralTypesResult = generateUsingSyntaxProperty(
+          PostModel,
+          factory.createTypeReferenceNode('Post'),
+          PostModel.slug,
+          true,
+        );
+
+        const typesResultStr = printNodes([singularTypesResult, pluralTypesResult]);
+
+        expect(typesResultStr).toMatchSnapshot();
+      });
+
+      test('a basic model with no link fields', () => {
+        const AccountModel = model({
+          slug: 'account',
+          pluralSlug: 'accounts',
+          fields: {
+            avatar: blob(),
+            email: string({ required: true }),
+            isActive: boolean(),
+            lastActiveAt: date(),
+            name: string(),
+            rewardPoints: number({ defaultValue: 0, required: true }),
+            settings: json({ defaultValue: {}, required: true }),
+          },
+        }) as unknown as Model;
+
+        const singularTypesResult = generateUsingSyntaxProperty(
+          AccountModel,
+          factory.createTypeReferenceNode('Account'),
+          AccountModel.slug,
+          false,
+        );
+        const pluralTypesResult = generateUsingSyntaxProperty(
+          AccountModel,
+          factory.createTypeReferenceNode('Account'),
+          AccountModel.slug,
+          true,
+        );
+
+        const typesResultStr = printNodes([singularTypesResult, pluralTypesResult]);
+
+        expect(typesResultStr).toMatchSnapshot();
+      });
+    });
+    describe('generate `with` syntax property', () => {
+      test('a basic model', () => {
+        const AccountModel = model({
+          slug: 'account',
+          pluralSlug: 'accounts',
+          fields: {
+            avatar: blob(),
+            email: string({ required: true }),
+            isActive: boolean(),
+            lastActiveAt: date(),
+            name: string(),
+            rewardPoints: number({ defaultValue: 0, required: true }),
+            settings: json({ defaultValue: {}, required: true }),
+          },
+        }) as unknown as Model;
+
+        const typesResult = generateWithSyntaxProperty(
+          AccountModel,
+          factory.createTypeReferenceNode('Account'),
+        );
+
+        const typesResultStr = printNodes([typesResult]);
+
+        expect(typesResultStr).toMatchSnapshot();
+      });
     });
   });
 });
