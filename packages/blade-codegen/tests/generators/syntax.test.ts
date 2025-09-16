0314d63f128623550c196416f716ba83cfcba883
nurodev <ben@nuro.dev>
2025-09-16 12:12:21 +0100

    Updated generator unit tests

---
+++ packages/blade-codegen/tests/generators/syntax.test.ts
@@ -0,0 +1,158 @@
+import { describe, expect, test } from 'bun:test';
+import { link, model, string } from 'blade-syntax/schema';
+import { factory } from 'typescript';
+
+import { generateUsingSyntax } from '@/src/generators/syntax';
+import { printNodes } from '@/src/utils/print';
+import { convertToPascalCase } from '@/src/utils/slug';
+
+import type { Model } from '@/src/types/model';
+
+describe('syntax', () => {
+  describe('using', () => {
+    const AccountModel = model({
+      slug: 'account',
+      pluralSlug: 'accounts',
+      fields: {
+        name: string(),
+      },
+    }) as unknown as Model;
+    const SingularAccountModelNode = factory.createUnionTypeNode([
+      factory.createTypeReferenceNode(convertToPascalCase(AccountModel.slug)),
+      factory.createLiteralTypeNode(factory.createNull()),
+    ]);
+    const PluralAccountModelNode = factory.createArrayTypeNode(
+      factory.createTypeReferenceNode(convertToPascalCase(AccountModel.slug)),
+    );
+
+    const PostModel = model({
+      slug: 'post',
+      pluralSlug: 'posts',
+      fields: {
+        title: string(),
+        author: link({ target: 'account' }),
+      },
+    }) as unknown as Model;
+    const SingularPostModelNode = factory.createUnionTypeNode([
+      factory.createTypeReferenceNode(convertToPascalCase(PostModel.slug)),
+      factory.createLiteralTypeNode(factory.createNull()),
+    ]);
+    const PluralPostModelNode = factory.createArrayTypeNode(
+      factory.createTypeReferenceNode(convertToPascalCase(PostModel.slug)),
+    );
+
+    describe('singular', () => {
+      describe('synchronous', () => {
+        test('with no link fields', () => {
+          const typesResult = generateUsingSyntax(
+            AccountModel,
+            SingularAccountModelNode,
+            false,
+            false,
+          );
+
+          const typesResultStr = printNodes([typesResult]);
+
+          expect(typesResultStr).toMatchSnapshot();
+        });
+
+        test('with a link field', () => {
+          const typesResult = generateUsingSyntax(
+            PostModel,
+            SingularPostModelNode,
+            false,
+            false,
+          );
+
+          const typesResultStr = printNodes([typesResult]);
+
+          expect(typesResultStr).toMatchSnapshot();
+        });
+      });
+      describe('asynchronous', () => {
+        test('with no link fields', () => {
+          const typesResult = generateUsingSyntax(
+            AccountModel,
+            SingularAccountModelNode,
+            true,
+            false,
+          );
+
+          const typesResultStr = printNodes([typesResult]);
+
+          expect(typesResultStr).toMatchSnapshot();
+        });
+
+        test('with a link field', () => {
+          const typesResult = generateUsingSyntax(
+            PostModel,
+            SingularPostModelNode,
+            true,
+            false,
+          );
+
+          const typesResultStr = printNodes([typesResult]);
+
+          expect(typesResultStr).toMatchSnapshot();
+        });
+      });
+    });
+
+    describe('plural', () => {
+      describe('synchronous', () => {
+        test('with no link fields', () => {
+          const typesResult = generateUsingSyntax(
+            AccountModel,
+            PluralAccountModelNode,
+            false,
+            true,
+          );
+
+          const typesResultStr = printNodes([typesResult]);
+
+          expect(typesResultStr).toMatchSnapshot();
+        });
+
+        test('with a link field', () => {
+          const typesResult = generateUsingSyntax(
+            PostModel,
+            PluralPostModelNode,
+            false,
+            true,
+          );
+
+          const typesResultStr = printNodes([typesResult]);
+
+          expect(typesResultStr).toMatchSnapshot();
+        });
+      });
+      describe('asynchronous', () => {
+        test('with no link fields', () => {
+          const typesResult = generateUsingSyntax(
+            AccountModel,
+            PluralAccountModelNode,
+            true,
+            true,
+          );
+
+          const typesResultStr = printNodes([typesResult]);
+
+          expect(typesResultStr).toMatchSnapshot();
+        });
+
+        test('with a link field', () => {
+          const typesResult = generateUsingSyntax(
+            PostModel,
+            PluralPostModelNode,
+            true,
+            true,
+          );
+
+          const typesResultStr = printNodes([typesResult]);
+
+          expect(typesResultStr).toMatchSnapshot();
+        });
+      });
+    });
+  });
+});
