a78dbdf87d1826ee616acfcfc6c247624502d174
Leo Lamprecht <leo@ronin.co>
2025-09-17 17:50:06 +0200

    Inline types

--- packages/blade/public/universal/types.ts
+++ packages/blade/public/universal/types.ts
@@ -32,5 +32,77 @@ export type {
 export type * from 'blade-client';
 export type * from 'blade-client/types';
 
-export type ReducedFunction = any;
-export type ResultRecord = any;
+// TODO: Import from compiler package.
+type ResultRecordBase<T extends Date | string = string> = {
+  /**
+   * The unique identifier of the record.
+   */
+  id: string;
+  ronin: {
+    /**
+     * The timestamp of when the record was created.
+     */
+    createdAt: T;
+    /**
+     * The ID of the user who created the record.
+     */
+    createdBy: string | null;
+    /**
+     * The timestamp of the last time the record was updated.
+     */
+    updatedAt: T;
+    /**
+     * The ID of the user who last updated the record.
+     */
+    updatedBy: string | null;
+  };
+};
+export type ResultRecord = Record<string, unknown> & ResultRecordBase;
+
+// TODO: Import from syntax package.
+/**
+ * Utility type to mark all Function.prototype methods as "deprecated" which
+ * deranks them in the IDE suggestion popup.
+ */
+export interface ReducedFunction {
+  /**
+   * @deprecated
+   */
+  name: any;
+  /**
+   * @deprecated
+   */
+  length: never;
+  /**
+   * @deprecated
+   */
+  apply: never;
+  /**
+   * @deprecated
+   */
+  call: never;
+  /**
+   * @deprecated
+   */
+  bind: never;
+  /**
+   * @deprecated
+   */
+  toString: never;
+  /**
+   * @deprecated
+   */
+  caller: never;
+  /**
+   * @deprecated
+   */
+  prototype: never;
+  /**
+   * @deprecated
+   */
+  arguments: never;
+  /**
+   * @deprecated
+   */
+  unify: never;
+}
