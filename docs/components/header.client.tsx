44b579e0c196b8de1241935ef67a655657e212fa
Leo Lamprecht <leo@ronin.co>
2025-09-22 11:49:23 +0200

    Bumped stars

--- docs/components/header.client.tsx
+++ docs/components/header.client.tsx
@@ -56,7 +56,7 @@ export const Header: FunctionComponent<{ nav: Array<NavGroup> }> = ({ nav }) => 
             rel="noreferrer">
             <Icons.Star className="size-3.5" />
 
-            <span>272</span>
+            <span>281</span>
           </a>
 
           <div className="block sm:hidden">
