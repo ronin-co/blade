5c47d48d5c85385d6c05aecc93894a2e3859c68a
Ben <4991309+NuroDev@users.noreply.github.com>
2025-10-02 17:03:21 +0100

    Ensure documentation external links don't use `NativeLink` (#533)

--- docs/components/link.tsx
+++ docs/components/link.tsx
@@ -1,11 +1,22 @@
 import { Link as NativeLink } from 'blade/client/components';
+import type { ComponentProps } from 'react';
 
-export const Link = ({ href, children }: { href: string; children: React.ReactNode }) => {
-  return (
-    <NativeLink href={href}>
-      <a className="text-cyan-600 transition-colors duration-200 hover:text-cyan-800">
+export const Link = ({ href, children }: ComponentProps<typeof NativeLink>) => {
+  const isExternal = typeof href === 'string' && href.startsWith('http');
+  if (isExternal)
+    return (
+      <a
+        className="text-cyan-600 transition-colors duration-200 hover:text-cyan-800"
+        href={href}>
         {children}
       </a>
+    );
+
+  return (
+    <NativeLink
+      className="text-cyan-600 transition-colors duration-200 hover:text-cyan-800"
+      href={href}>
+      {children}
     </NativeLink>
   );
 };
