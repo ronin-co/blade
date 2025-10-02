26de11ea91f045bce5484722ab2cdbe677013a1d
nurodev <ben@nuro.dev>
2025-10-02 16:52:03 +0100

    Added external link check to `docs` `Link` component

--- docs/components/link.tsx
+++ docs/components/link.tsx
@@ -2,6 +2,18 @@ import { Link as NativeLink } from 'blade/client/components';
 import type { ComponentProps } from 'react';
 
 export const Link = ({ href, children }: ComponentProps<typeof NativeLink>) => {
+  const isExternal = typeof href === 'string' && href.startsWith('http');
+  if (isExternal)
+    return (
+      <a
+        className="text-cyan-600 transition-colors duration-200 hover:text-cyan-800"
+        href={href}
+        rel="noopener noreferrer"
+        target="_blank">
+        {children}
+      </a>
+    );
+
   return (
     <NativeLink
       className="text-cyan-600 transition-colors duration-200 hover:text-cyan-800"
