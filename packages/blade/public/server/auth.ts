b9502d0ba5a3f995927421d9361c76dc1f316e2c
Leo Lamprecht <leo@ronin.co>
2025-10-08 12:03:10 +0200

    Export session schema

--- packages/blade/public/server/auth.ts
+++ packages/blade/public/server/auth.ts
@@ -1,4 +1,4 @@
-export { Account } from 'blade-auth/schema';
+export { Account, Session } from 'blade-auth/schema';
 export { set as setAccount, add as addAccount } from 'blade-auth/triggers/account';
 export {
   get as getSession,
