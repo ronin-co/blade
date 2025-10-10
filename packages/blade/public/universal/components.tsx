edb93128e95d2e02daac55fc6c346de6a8898067
Leo Lamprecht <leo@ronin.co>
2025-10-10 16:13:20 +0200

    Export `FormElement` component

--- packages/blade/public/universal/components.tsx
+++ packages/blade/public/universal/components.tsx
@@ -1,4 +1,5 @@
 export { Link } from '@/private/client/components/link';
 export { Image } from '@/private/client/components/image';
 export { Form, FormContext } from '@/private/client/components/form';
+export { FormElement } from '@/private/client/components/form-element';
 export { Input } from '@/private/client/components/input';
