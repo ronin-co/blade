051376470f2c69141f68a3a3d6ccb561623d9d6c
Leo Lamprecht <leo@ronin.co>
2025-10-26 16:21:34 +0100

    Fixed HMR warning

--- packages/blade/private/client/utils/page.ts
+++ packages/blade/private/client/utils/page.ts
@@ -32,7 +32,7 @@ const loadResource = async (bundleId: string, type: PreloadableAsset) => {
   return new Promise((resolve, reject) => {
     const link = document.createElement('link') as HTMLLinkElement;
 
-    link.rel = 'preload';
+    link.rel = extension === 'css' ? 'preload' : 'modulepreload';
     link.as = extension === 'css' ? 'style' : 'script';
     link.onload = resolve;
     link.onerror = reject;
