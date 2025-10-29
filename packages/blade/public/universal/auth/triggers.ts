9f10352ce93525fdba4d4f209157ad8e7758419b
Leo Lamprecht <leo@ronin.co>
2025-10-29 18:13:32 +0100

    Fixed mistake

--- packages/blade/public/universal/auth/triggers.ts
+++ packages/blade/public/universal/auth/triggers.ts
@@ -1,4 +1,4 @@
 import getAccountTriggers from 'blade-auth/triggers/account';
-import getSessionTriggers from 'blade-auth/triggers/account';
+import getSessionTriggers from 'blade-auth/triggers/session';
 
 export { getAccountTriggers, getSessionTriggers };
