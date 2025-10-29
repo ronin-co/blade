4b47f972b5750a68d37cdc3589f26a0a90b3834b
Leo Lamprecht <leo@ronin.co>
2025-10-29 18:06:38 +0100

    Refine exports

---
+++ packages/blade/public/universal/auth/triggers.ts
@@ -0,0 +1,4 @@
+import getAccountTriggers from 'blade-auth/triggers/account';
+import getSessionTriggers from 'blade-auth/triggers/account';
+
+export { getAccountTriggers, getSessionTriggers };
--- packages/blade/public/universal/auth/triggers/account.ts
+++
@@ -1,1 +0,0 @@
-export * from 'blade-auth/triggers/account';
