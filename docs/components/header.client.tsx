2a46c590e548c2862cca401afa9b84f76721ce4d
Leo Lamprecht <leo@ronin.co>
2025-11-23 13:13:56 +0100

    Update stars

--- docs/components/header.client.tsx
+++ docs/components/header.client.tsx
@@ -56,7 +56,7 @@ export const Header: FunctionComponent<{ nav: Array<NavGroup> }> = ({ nav }) => 
             rel="noreferrer">
             <Icons.Star className="size-3.5" />
 
-            <span>289</span>
+            <span>317</span>
           </a>
 
           <div className="block sm:hidden">
