3fc40387b2ceacc956b653715026b03c2382225e
Leo Lamprecht <leo@ronin.co>
2025-12-08 11:56:37 +0100

    Upgrade dependencies

--- packages/blade-client/tsdown.config.ts
+++ packages/blade-client/tsdown.config.ts
@@ -4,9 +4,7 @@ export default defineConfig({
   dts: true,
   entry: [
     'src/index.ts',
-    'src/bin/index.ts',
     'src/types/index.ts',
-    'src/schema/index.ts',
     'src/utils/index.ts',
   ],
   format: 'esm',
